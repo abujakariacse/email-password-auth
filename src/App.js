9f926a9342a170fb833615ed27e121b8573e5e27
abujakariacse <abujakaria316@gmail.com>
2022-05-02 00:19:06 +0600

    displayName added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import app from './firebase.init';
-import { createUserWithEmailAndPassword, getAuth, sendEmailVerification, signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, sendEmailVerification, sendPasswordResetEmail, signInWithEmailAndPassword, updateProfile } from "firebase/auth";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
@@ -15,17 +15,16 @@ function App() {
   // For Form Validation
   const [validated, setValidated] = useState(false);
   const [error, setError] = useState('');
+  const [name, setName] = useState('');
 
 
   const handleOnSubmit = (e) => {
     e.preventDefault();
     // Form Validation Code Below
     const form = e.currentTarget;
-    if (form.checkValidity() === false && !/(?=.* [!@#$%^&*])/.test(password)) {
+    if (form.checkValidity() === false) {
       e.preventDefault();
       e.stopPropagation();
-      setError('Please include atleast one special charecter');
-      return;
     }
 
 
@@ -39,7 +38,7 @@ function App() {
           console.log(user);
         })
         .catch(error => {
-          console.error(error.message);
+          setError(error.message);
         })
     }
     else {
@@ -48,12 +47,23 @@ function App() {
           const user = result.user;
           console.log(user);
           verifyEmail();
+
+          updateProfile(auth.currentUser, { displayName: name })
+            .then(() => {
+              console.log('Name Updated')
+            })
+            .then(error => {
+              console.log(error)
+            })
         })
         .catch(error => {
           console.log(error.message);
         })
 
     }
+  }
+  const handleName = (e) => {
+    setName(e.target.value);
   }
 
   const handleEmailBlur = (e) => {
@@ -76,11 +86,25 @@ function App() {
       })
 
   }
+  const handlePasswordReset = () => {
+    !email || sendPasswordResetEmail(auth, email)
+      .then(() => {
+        console.log("Reset email sent");
+      })
+      .catch(error => {
+        setError(error)
+      })
+  }
+
   return (
     <div className="App">
       <div className='w-50 mx-auto mt-5'>
         <h3 className='text-primary'>Please {registerd ? 'Login' : 'Register'}</h3>
         <Form noValidate validated={validated} onSubmit={handleOnSubmit}>
+          {!registerd ? <Form.Group className="mb-3" controlId="formBasicText">
+            <Form.Label>Name</Form.Label>
+            <Form.Control onBlur={handleName} type="text" placeholder="Your Name" required />
+          </Form.Group> : ''}
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
             <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" required />
@@ -91,9 +115,9 @@ function App() {
             <Form.Control onBlur={handlePasswordBlur} type="password" placeholder="Password" required />
             <p className='text-danger'>{error}</p>
           </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Group className="mb-3 d-flex align-items-center" controlId="formBasicCheckbox">
             <Form.Check onChange={handleCheckbox} type="checkbox" label="Already Registerd?" />
-            <Button onClick={handlePasswordReset} variant="link">Link</Button>
+            <Button onClick={handlePasswordReset} variant="link" className='text-danger'>Forgot Password?</Button>
           </Form.Group>
           <Button variant="primary" type="submit">
             {registerd ? 'Login' : 'Register'}
